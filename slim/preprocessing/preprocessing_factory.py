3d3e855c2e5f634b6bddc353b6830f01f120bfe1
서일근 <seoilgun@gmail.com>
2016-12-12 23:39:44 +0900

    add koreans preprocessing

--- slim/preprocessing/preprocessing_factory.py
+++ slim/preprocessing/preprocessing_factory.py
@@ -24,6 +24,7 @@ from preprocessing import cifarnet_preprocessing
 from preprocessing import inception_preprocessing
 from preprocessing import lenet_preprocessing
 from preprocessing import vgg_preprocessing
+from preprocessing import koreans_preprocessing
 
 slim = tf.contrib.slim
 
@@ -45,21 +46,22 @@ def get_preprocessing(name, is_training=False):
     ValueError: If Preprocessing `name` is not recognized.
   """
   preprocessing_fn_map = {
-      'cifarnet': cifarnet_preprocessing,
-      'inception': inception_preprocessing,
-      'inception_v1': inception_preprocessing,
-      'inception_v2': inception_preprocessing,
-      'inception_v3': inception_preprocessing,
-      'inception_v4': inception_preprocessing,
-      'inception_resnet_v2': inception_preprocessing,
-      'lenet': lenet_preprocessing,
-      'resnet_v1_50': vgg_preprocessing,
-      'resnet_v1_101': vgg_preprocessing,
-      'resnet_v1_152': vgg_preprocessing,
-      'vgg': vgg_preprocessing,
-      'vgg_a': vgg_preprocessing,
-      'vgg_16': vgg_preprocessing,
-      'vgg_19': vgg_preprocessing,
+    'cifarnet': cifarnet_preprocessing,
+    'inception': inception_preprocessing,
+    'inception_v1': inception_preprocessing,
+    'inception_v2': inception_preprocessing,
+    'inception_v3': inception_preprocessing,
+    'inception_v4': inception_preprocessing,
+    'inception_resnet_v2': inception_preprocessing,
+    'lenet': lenet_preprocessing,
+    'resnet_v1_50': vgg_preprocessing,
+    'resnet_v1_101': vgg_preprocessing,
+    'resnet_v1_152': vgg_preprocessing,
+    'vgg': vgg_preprocessing,
+    'vgg_a': vgg_preprocessing,
+    'vgg_16': vgg_preprocessing,
+    'vgg_19': vgg_preprocessing,
+    'koreans': koreans_preprocessing,
   }
 
   if name not in preprocessing_fn_map:
@@ -67,6 +69,6 @@ def get_preprocessing(name, is_training=False):
 
   def preprocessing_fn(image, output_height, output_width, **kwargs):
     return preprocessing_fn_map[name].preprocess_image(
-        image, output_height, output_width, is_training=is_training, **kwargs)
+      image, output_height, output_width, is_training=is_training, **kwargs)
 
   return preprocessing_fn
