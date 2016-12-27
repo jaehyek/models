cc15377d329cf2a7fe374bd129d43d420b4c918b
서일근 <seoilgun@gmail.com>
2016-12-27 17:56:25 +0900

    add chatbot

--- tutorials/rnn/chatbot/seq2seq_model.py
+++ tutorials/rnn/chatbot/seq2seq_model.py
@@ -15,8 +15,8 @@
 
 """Sequence-to-sequence model with an attention mechanism."""
 
-# from __future__ import absolute_import
-# from __future__ import division
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
 
 import random
@@ -25,7 +25,7 @@ import numpy as np
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
-import data_utils
+from tensorflow.models.rnn.translate import data_utils
 
 
 class Seq2SeqModel(object):
