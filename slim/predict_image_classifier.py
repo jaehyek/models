68624367443a82163e9fed6143405ad8345e893e
서일근 <seoilgun@gmail.com>
2017-02-10 17:36:56 +0900

    dd

--- slim/predict_image_classifier.py
+++ slim/predict_image_classifier.py
@@ -25,6 +25,7 @@ from datasets import dataset_factory
 from nets import nets_factory
 from preprocessing import preprocessing_factory
 import numpy as np
+from inception.slim import variables
 
 slim = tf.contrib.slim
 
@@ -136,12 +137,26 @@ def main(_):
       num_threads=FLAGS.num_preprocessing_threads,
       capacity=5 * FLAGS.batch_size)
 
-    tf.image_summary('test_images', images, FLAGS.batch_size)
+    # tf.image_summary('test_images', images, FLAGS.batch_size)
 
     ####################
     # Define the model #
     ####################
     logits, _ = network_fn(images)
+
+    with tf.variable_scope('resnet_v2_152/block1/unit_1/bottleneck_v2/conv1', reuse=True):
+      weights = tf.get_variable('weights')
+      with tf.variable_scope('visualization'):
+        # scale weights to [0 1], type is still float
+        x_min = tf.reduce_min(weights)
+        x_max = tf.reduce_max(weights)
+        kernel_0_to_1 = (weights - x_min) / (x_max - x_min)
+
+        # to tf.image_summary format [batch_size, height, width, channels]
+        kernel_transposed = tf.transpose(kernel_0_to_1, [3, 0, 1, 2])
+
+        # this will display random 3 filters from the 64 in conv1
+        tf.image_summary('conv1/filters', kernel_transposed, max_images=50)
 
     if FLAGS.moving_average_decay:
       variable_averages = tf.train.ExponentialMovingAverage(
@@ -201,6 +216,8 @@ def main(_):
     # print(final_op_value)
     print(result_predict)
     print(np.argsort(result_predict[:, 1])[-5:])
+
+    weights = variables.variable('weights')
 
 
 if __name__ == '__main__':
